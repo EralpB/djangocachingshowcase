508653ab990a1cae2095609cfcd595073f361693
Eralp Bayraktar <2274357+EralpB@users.noreply.github.com>
2020-05-17 10:29:16 +0200

    V2: Fixing stale/nonexisting objects.

--- bookshop/models.py
+++ bookshop/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.cache import cache
 import time
 
+
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
@@ -9,11 +10,12 @@ class Author(models.Model):
         cache_key = 'Author:get_top_books:{}'.format(self.id)
         cache_value = cache.get(cache_key)
         if cache_value is not None:
-            return cache_value
+            return list(Book.objects.filter(id__in=cache_value))
 
         time.sleep(5)  # make the calculation slower for the sake of argument
         books = list(Book.objects.order_by('-purchase_count')[:10])
-        cache.set(cache_key, books, 4 * 60 * 60)  # cache for 4 hours
+        # cache.set(cache_key, books, 4 * 60 * 60)  # cache for 4 hours
+        cache.set(cache_key, [book.id for book in books], 4 * 60 * 60)  # cache for 4 hours
         return books
 
     def __str__(self):
