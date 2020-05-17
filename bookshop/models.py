f28e8098074f0b7a65db7c2de7474fafbea77004
Eralp Bayraktar <2274357+EralpB@users.noreply.github.com>
2020-05-17 10:23:20 +0200

    V1: Manual Caching.

--- bookshop/models.py
+++ bookshop/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.core.cache import cache
 import time
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
     def get_top_books(self):
-        time.sleep(5)
-        return list(Book.objects.filter(author=self).order_by('-purchase_count')[:10])
+        cache_key = 'Author:get_top_books:{}'.format(self.id)
+        cache_value = cache.get(cache_key)
+        if cache_value is not None:
+            return cache_value
+
+        time.sleep(5)  # make the calculation slower for the sake of argument
+        books = list(Book.objects.order_by('-purchase_count')[:10])
+        cache.set(cache_key, books, 4 * 60 * 60)  # cache for 4 hours
+        return books
 
     def __str__(self):
         return self.name
