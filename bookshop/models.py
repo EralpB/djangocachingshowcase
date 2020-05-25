832647dcedd2bc70d8fdeaffe01ab8cdc3b39496
Eralp Bayraktar <2274357+EralpB@users.noreply.github.com>
2020-05-25 11:26:58 +0200

    Use django function caching.

--- bookshop/models.py
+++ bookshop/models.py
@@ -1,56 +1,19 @@
 from django.db import models
-from django.core.cache import cache
 import time
-import redis
-import json
 
-r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+from functioncaching import cached_function
 
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
-    # _ indicates this function is for internal use only
-    def _get_top_books(self):
+    @cached_function(timeout=24*60*60, freshness_timeout=60*60)
+    def _get_top_book_ids(self):
         time.sleep(5)  # make the calculation slower for the sake of argument
-        return list(Book.objects.order_by('-purchase_count')[:10])
-
+        return list(Book.objects.filter(author=self).order_by('-purchase_count').values_list('id', flat=True)[:10])
 
     def get_top_books(self):
-        CACHE_TIMEOUT = 24 * 60 * 60  # 1 day
-        CACHE_FRESHNESS = 60 * 60  # 1 hour
-        REMAINING_CUTOFF = CACHE_TIMEOUT - CACHE_FRESHNESS  # If TTL less than this value, needs recalculate
-
-        cache_key = 'Author:get_top_books:{}'.format(self.id)
-        lock_key = 'Lock:{}'.format(cache_key)
-        cache_value = r.get(cache_key)
-        if cache_value is not None:
-            cache_value = json.loads(cache_value)
-        remaining_ttl = r.ttl(cache_key)
-
-        should_recalculate = remaining_ttl < REMAINING_CUTOFF
-
-        if not should_recalculate and cache_value is not None:
-            # if key is fresh enough, and value exists, just return!
-            return list(Book.objects.filter(id__in=cache_value))
-
-        # try to acquire lock to recalculate..
-        try:
-            with r.lock(lock_key, timeout=60, blocking_timeout=0):
-                books = self._get_top_books()
-        except redis.exceptions.LockError:
-            # somebody else is calculating, if cache value exists, no problem, no error
-            if cache_value is not None:
-                return list(Book.objects.filter(id__in=cache_value))
-            else:
-                # we don't even have a stale cache, and some worker is calculating, no choice but to error out
-                raise Exception('ColdCacheException')
-
-        # only update the cache if you freshly calculated!
-        # In other cases the execution doesnt reach here, it either gives exceptions or returns early.
-        cache_representation = json.dumps([book.id for book in books])
-        r.set(cache_key, cache_representation, CACHE_TIMEOUT)  # cache for 1 day
-        return books
+        return list(Book.objects.filter(id__in=self._get_top_book_ids()))
 
     def __str__(self):
         return self.name
--- cachingshowcase/settings.py
+++ cachingshowcase/settings.py
@@ -119,3 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
